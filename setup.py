1c3c49ea4ab11713bf9fab5aa2e8fd754f77c3b9
palkeo <palkeo@gmail.com>
2013-06-28 14:44:29 +0200

    Added files for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'django-safedelete',
+    packages = ['django-safedelete'],
+    version = '0.1',
+    description = 'Make you objects invisible instead of deleting them from your database.',
+    author = 'Korantin Auguste',
+    author_email = 'contact@palkeo.com',
+    url = 'https://github.com/makinacorpus/django-safedelete',
+    download_url = 'https://github.com/makinacorpus/django-safedelete/tarball/0.1',
+    keywords = ['django', 'delete', 'safedelete', 'softdelete'],
+    classifiers = [],
+)
