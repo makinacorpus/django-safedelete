ab1eec1d4907b0d8d8a77032701a50730e3315a5
palkeo <palkeo@gmail.com>
2013-07-04 11:57:23 +0200

    Init modified for pyflakes.

--- safedelete/__init__.py
+++ safedelete/__init__.py
@@ -1,4 +1,13 @@
+# flake8: noqa
+
 from .utils import (HARD_DELETE, SOFT_DELETE, HARD_DELETE_NOCASCADE,
-    DELETED_INVISIBLE, DELETED_VISIBLE_BY_PK)
+                    DELETED_INVISIBLE, DELETED_VISIBLE_BY_PK)
 from .models import safedelete_mixin_factory
 from .managers import safedelete_manager_factory
+
+__all__ = ['safedelete_manager_factory',
+           'safedelete_mixin_factory',
+           'HARD_DELETE', 'SOFT_DELETE',
+           'HARD_DELETE_NOCASCADE',
+           'DELETED_INVISIBLE',
+           'DELETED_VISIBLE_BY_PK']
