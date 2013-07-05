f54c5fc1d935945581426cbf5504f2ac46167082
palkeo <palkeo@gmail.com>
2013-07-05 11:48:02 +0200

    Good setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from distutils.core import setup
+
+version = "0.1.1"
+
 setup(
     name = 'django-safedelete',
     packages = ['django-safedelete'],
-    version = '0.1',
+    version = version,
     description = 'Make you objects invisible instead of deleting them from your database.',
     author = 'Korantin Auguste',
     author_email = 'contact@palkeo.com',
     url = 'https://github.com/makinacorpus/django-safedelete',
-    download_url = 'https://github.com/makinacorpus/django-safedelete/tarball/0.1',
+    download_url = 'https://github.com/makinacorpus/django-safedelete/tarball/%s' % version,
     keywords = ['django', 'delete', 'safedelete', 'softdelete'],
-    classifiers = [],
+    classifiers = [
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 4 - Beta",
+    ],
 )
