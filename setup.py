fd48926fed55223d11623cb87de1e27f01318ff7
palkeo <contact@palkeo.com>
2014-05-15 12:27:03 +0200

    Version change (and deletion of the VERSION file).

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.rst', 'r') as f:
     long_description = f.read()
 
-version = "0.1.2"
+version = "0.1.3"
 
 setup(
     name = 'django-safedelete',
