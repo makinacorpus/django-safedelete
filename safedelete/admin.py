245e102c6ee56010438c17b655b6383b264697c6
Gagaro <gagaro42@gmail.com>
2014-10-02 16:56:09 +0200

    Feat: add ModelAdmin to show deleted objects

---
+++ safedelete/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+
+
+class SafeDeleteAdmin(admin.ModelAdmin):
+    """
+        from safedelete import admin.SafeDeleteAdmin
+        class ContactAdmin(SafeDeleteAdmin):
+            list_display = ("first_name", "last_name", "email") \
+                + SafeDeleteAdmin.list_display
+            list_filter = ("last_name") + SafeDeleteAdmin.list_filter
+    """
+    list_display = ('deleted',)
+    list_filter = ('deleted',)
+
+    class Meta:
+        abstract = True
+
+    def queryset(self, request):
+        try:
+            qs = self.model._default_manager.all_with_deleted()
+        except:
+            qs = self.model._default_manager.all()
+
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
