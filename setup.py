6b92f603c7be496d13762ce6bbc5cad873538a73
Florent Lebreton <florentlebreton@free.fr>
2014-12-09 17:02:11 +0100

    Prepare release 0.2.0

--- setup.py
+++ setup.py
@@ -1,28 +1,53 @@
+import codecs
+import os
+import re
+
 from setuptools import setup, find_packages
 
-with open('README.rst', 'r') as f:
-    long_description = f.read()
+here = os.path.abspath(os.path.dirname(__file__))
 
-version = "0.1.3"
+with open('README.rst', 'r') as f_readme:
+    with open('CHANGES', 'r') as f_changes:
+        long_description = f_readme.read() + '\n\n' + f_changes.read()
+
+
+def get_version(package_name):
+    version_re = re.compile(r"^__version__ = [\"']([\w_.-]+)[\"']$")
+    package_components = package_name.split('.')
+    init_path = os.path.join(here, *(package_components + ['__init__.py']))
+    with codecs.open(init_path, 'r', 'utf-8') as f:
+        for line in f:
+            match = version_re.match(line[:-1])
+            if match:
+                return match.groups()[0]
+    raise RuntimeError("Unable to find version string.")
+
+version = get_version('safedelete')
 
 setup(
-    name = 'django-safedelete',
-    packages = find_packages(),
-    version = version,
-    description = 'Mask your objects instead of deleting them from your database.',
-    long_description = long_description,
-    author = 'Korantin Auguste',
-    author_email = 'contact@palkeo.com',
-    url = 'https://github.com/makinacorpus/django-safedelete',
-    download_url = 'https://github.com/makinacorpus/django-safedelete/tarball/%s' % version,
-    keywords = ['django', 'delete', 'safedelete', 'softdelete'],
-    classifiers = [
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 2.7",
-        "Development Status :: 4 - Beta",
+    name='django-safedelete',
+    packages=find_packages(),
+    version=version,
+    description='Mask your objects instead of deleting them from your database.',
+    long_description=long_description,
+    author='Korantin Auguste',
+    author_email='contact@palkeo.com',
+    url='https://github.com/makinacorpus/django-safedelete',
+    download_url='https://github.com/makinacorpus/django-safedelete/tarball/%s' % version,
+    keywords=['django', 'delete', 'safedelete', 'softdelete'],
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+        'Development Status :: 4 - Beta',
     ],
     license='BSD',
     requires=['Django (>= 1.3)'],
