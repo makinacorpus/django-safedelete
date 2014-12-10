77f8841a4ca0f62485155b86bf1e85534c94e932
Florent Lebreton <florentlebreton@free.fr>
2014-12-10 10:51:25 +0100

    Fix setup.py and MANIFEST.in

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.4',
         'Development Status :: 4 - Beta',
     ],
     license='BSD',
