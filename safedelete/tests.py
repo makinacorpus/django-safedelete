757807adce79ab1492885e5854050a761c240cb8
Giba <gilberto.lira@gmail.com>
2016-11-08 06:48:23 -0200

    Dynamic model superclass (#50)

--- safedelete/tests.py
+++ safedelete/tests.py
@@ -86,6 +86,20 @@ class HasCustomQueryset(safedelete_mixin_factory(
     color = models.CharField(max_length=5, choices=(('red', 'Red'), ('green', 'Green')))
 
 
+class CustomModel(models.Model):
+    def customMethod(self):
+        pass
+
+
+class WrongEdition(CustomModel, SoftDeleteMixin):
+    name = models.CharField(max_length=100)
+
+
+class Edition(safedelete_mixin_factory(policy=SOFT_DELETE,
+                                       model_superclass=CustomModel)):
+    name = models.CharField(max_length=100)
+
+
 class NameVisibleField(safedelete_mixin_factory(SOFT_DELETE, visibility=DELETED_VISIBLE_BY_PK,
                        visibility_field="name")):
     name = models.CharField(max_length=200, unique=True)
@@ -365,6 +379,14 @@ class SimpleTest(TestCase):
         Category.objects.create(name='test').delete()
         with self.assertRaises(ValidationError):
             Category(name='test').validate_unique()
+
+    def test_custom_model_fail(self):
+        self.assertTrue(hasattr(WrongEdition, "customMethod"))
+        self.assertFalse(hasattr(WrongEdition.objects, "all_with_deleted"))
+
+    def test_custom_model_ok(self):
+        self.assertTrue(hasattr(Edition, "customMethod"))
+        self.assertTrue(hasattr(Edition.objects, "all_with_deleted"))
 
 
 class AdminTestCase(TestCase):
