22d5b28f1ab55c5657ad2b3f9ad69f1067680e3b
Andreas Backx <andreas@backx.org>
2016-12-22 00:27:41 +0100

    Fixed bug where related ForeignKeys would still be hard deleted.

--- safedelete/utils.py
+++ safedelete/utils.py
@@ -1,6 +1,5 @@
-from django.db.models.deletion import Collector
 from django.db import router
-
+from django.db.models.deletion import Collector
 
 HARD_DELETE = 0
 SOFT_DELETE = 1
@@ -15,4 +14,4 @@ DELETED_VISIBLE_BY_PK = 11
 def can_hard_delete(obj):
     collector = Collector(using=router.db_for_write(obj))
     collector.collect([obj])
-    return not bool(collector.fast_deletes)
+    return not bool(collector.dependencies)
