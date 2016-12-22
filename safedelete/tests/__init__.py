df37521978aafda853f530ac8c697b74b931165c
Andreas Backx <andreas@backx.org>
2016-12-22 14:41:50 +0100

    Refactored test_softdelete_signals.

--- safedelete/tests/__init__.py
+++ safedelete/tests/__init__.py
@@ -6,15 +6,14 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.test import TestCase, RequestFactory
 
-from .admin import SafeDeleteAdmin, highlight_deleted
-from .utils import (
+from ..admin import SafeDeleteAdmin, highlight_deleted
+from ..utils import (
     HARD_DELETE,
     HARD_DELETE_NOCASCADE, SOFT_DELETE, NO_DELETE,
     DELETED_VISIBLE_BY_PK
 )
-from .managers import SafeDeleteManager, SafeDeleteQueryset
-from .models import SafeDeleteMixin
-from .signals import post_softdelete, post_undelete
+from ..managers import SafeDeleteManager, SafeDeleteQueryset
+from ..models import SafeDeleteMixin
 
 
 # MODELS (FOR TESTING)
@@ -148,44 +147,6 @@ class SimpleTest(TestCase):
         self.order.save()
 
         self.assertEqual(Order.objects.count(), 1)
-
-    def test_softdelete_signals(self):
-        # prevents shadowing...
-        class TestObj(object):
-            received_softdelete_signal = False
-            received_softcreate_signal = False
-
-            def receive_softdelete(self, sender, instance, **kwargs):
-                self.received_softdelete_signal = True
-
-            def receive_softcreate(self, sender, instance, **kwargs):
-                self.received_softcreate_signal = True
-
-        test_obj = TestObj()
-
-        post_softdelete.connect(test_obj.receive_softdelete, sender=Order)
-        post_undelete.connect(test_obj.receive_softcreate, sender=Order)
-
-        self.assertEqual(Order.objects.count(), 1)
-
-        self.order.delete()
-
-        self.assertEqual(Order.objects.count(), 0)
-        self.assertEqual(Order.objects.all_with_deleted().count(), 1)
-        self.assertTrue(test_obj.received_softdelete_signal)
-        self.assertFalse(test_obj.received_softcreate_signal)
-
-        test_obj.received_softdelete_signal = False
-        test_obj.received_softcreate_signal = False
-
-        self.order.save()
-
-        self.assertEqual(Order.objects.count(), 1)
-        self.assertFalse(test_obj.received_softdelete_signal)
-        self.assertTrue(test_obj.received_softcreate_signal)
-
-        post_softdelete.disconnect(test_obj.receive_softdelete, sender=Order)
-        post_undelete.disconnect(test_obj.receive_softcreate, sender=Order)
 
     def test_hard_delete(self):
         self.assertEqual(Article.objects.count(), 3)
