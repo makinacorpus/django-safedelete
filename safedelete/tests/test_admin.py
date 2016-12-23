05cf38b6286dd57fbae434d1888d9f023de26d79
Andreas Backx <andreas@backx.org>
2016-12-23 01:10:08 +0100

    Refactored admin tests.

--- safedelete/tests/test_admin.py
+++ safedelete/tests/test_admin.py
@@ -7,18 +7,13 @@ from django.test import RequestFactory, TestCase
 
 from ..admin import SafeDeleteAdmin, highlight_deleted
 from ..config import (DELETED_VISIBLE_BY_PK, HARD_DELETE,
-                      HARD_DELETE_NOCASCADE, NO_DELETE, SOFT_DELETE)
-from ..managers import SafeDeleteManager, SafeDeleteQueryset
+                      HARD_DELETE_NOCASCADE, SOFT_DELETE)
+from ..managers import SafeDeleteManager
 from ..models import SafeDeleteMixin
-
-
-# MODELS (FOR TESTING)
 
 
 class Author(SafeDeleteMixin):
     _safedelete_policy = HARD_DELETE_NOCASCADE
-
-    name = models.CharField(max_length=200)
 
 
 class CategoryManager(SafeDeleteManager):
@@ -28,60 +23,30 @@ class CategoryManager(SafeDeleteManager):
 class Category(SafeDeleteMixin):
     _safedelete_policy = SOFT_DELETE
 
-    name = models.CharField(max_length=200, unique=True)
-
+    name = models.CharField(
+        max_length=100,
+        blank=True
+    )
     objects = CategoryManager()
-
-    def __str__(self):
-        return self.name
 
 
 class Article(SafeDeleteMixin):
     _safedelete_policy = HARD_DELETE
 
-    name = models.CharField(max_length=200)
-    author = models.ForeignKey(Author)
-    category = models.ForeignKey(Category, null=True, default=None)
-
-    def __unicode__(self):
-        return 'Article ({0}): {1}'.format(self.pk, self.name)
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        null=True,
+        default=None
+    )
 
 
 class Order(SafeDeleteMixin):
-    name = models.CharField(max_length=100)
     articles = models.ManyToManyField(Article)
-
-
-class VeryImportant(SafeDeleteMixin):
-    _safedelete_policy = NO_DELETE
-
-    name = models.CharField(max_length=200)
-
-
-class CustomQueryset(SafeDeleteQueryset):
-
-    def best(self):
-        return self.filter(color='green')
-
-
-class CustomManager(SafeDeleteManager):
-
-    def get_queryset(self):
-        queryset = CustomQueryset(self.model, using=self._db)
-        return queryset.filter(deleted__isnull=True)
-
-    def best(self):
-        return self.get_queryset().best()
-
-
-class HasCustomQueryset(SafeDeleteMixin):
-    name = models.CharField(max_length=200)
-    color = models.CharField(max_length=5, choices=(('red', 'Red'), ('green', 'Green')))
-
-    objects = CustomManager()
-
-
-# ADMINMODEL (FOR TESTING)
 
 
 class CategoryAdmin(SafeDeleteAdmin):
@@ -91,30 +56,43 @@ class CategoryAdmin(SafeDeleteAdmin):
 admin.site.register(Category, CategoryAdmin)
 
 
-# TESTS
-
 class AdminTestCase(TestCase):
     urls = 'safedelete.tests.urls'
 
     def setUp(self):
-        self.author = Author.objects.create(name='author 0')
+        self.author = Author.objects.create(
+        )
+
         self.categories = (
-            Category.objects.create(name='category 0'),
-            Category.objects.create(name='category 1'),
-            Category.objects.create(name='category 2'),
+            Category.objects.create(),
+            Category.objects.create(),
+            Category.objects.create(),
         )
+
         self.articles = (
-            Article(name='article 0', author=self.author),
-            Article(name='article 1', author=self.author, category=self.categories[1]),
-            Article(name='article 2', author=self.author, category=self.categories[2]),
+            Article(
+                author=self.author
+            ),
+            Article(
+                author=self.author,
+                category=self.categories[1]
+            ),
+            Article(
+                author=self.author,
+                category=self.categories[2]
+            ),
         )
+
         self.categories[1].delete()
+
         self.request_factory = RequestFactory()
         self.request = self.request_factory.get('/', {})
+
         self.modeladmin_default = admin.ModelAdmin(Category, AdminSite())
         self.modeladmin = CategoryAdmin(Category, AdminSite())
-        User.objects.create_superuser("super", "", "secret")
-        self.client.login(username="super", password="secret")
+
+        User.objects.create_superuser('super', 'email@domain.com', 'secret')
+        self.client.login(username='super', password='secret')
 
     def tearDown(self):
         self.client.logout()
@@ -132,31 +110,34 @@ class AdminTestCase(TestCase):
     def test_admin_model(self):
         changelist_default = self.get_changelist(self.request, Category, self.modeladmin_default)
         changelist = self.get_changelist(self.request, Category, self.modeladmin)
-        self.assertEqual(changelist.get_filters(self.request)[0][0].title, "deleted")
+        self.assertEqual(changelist.get_filters(self.request)[0][0].title, 'deleted')
         self.assertEqual(changelist.queryset.count(), 3)
         self.assertEqual(changelist_default.queryset.count(), 2)
 
     def test_admin_listing(self):
-        """ Test deleted objects are in red in admin listing. """
+        """Test deleted objects are in red in admin listing."""
         resp = self.client.get('/admin/safedelete/category/')
         line = '<span class="deleted">{0}</span>'.format(self.categories[1])
         self.assertContains(resp, line)
 
     def test_admin_xss(self):
+        """Test whether admin XSS is blocked."""
         Category.objects.create(name='<script>alert(42)</script>'),
         resp = self.client.get('/admin/safedelete/category/')
         # It should be escaped
         self.assertNotContains(resp, '<script>alert(42)</script>')
 
     def test_admin_undelete_action(self):
-        """ Test objects are undeleted and action is logged. """
+        """Test objects are undeleted and action is logged."""
         resp = self.client.post('/admin/safedelete/category/', data={
             'index': 0,
             'action': ['undelete_selected'],
             '_selected_action': [self.categories[1].pk],
         })
         self.assertTemplateUsed(resp, 'safedelete/undelete_selected_confirmation.html')
-        category = Category.objects.get(pk=self.categories[1].pk)
+        category = Category.objects.get(
+            pk=self.categories[1].pk
+        )
         self.assertTrue(self.categories[1].deleted)
 
         resp = self.client.post('/admin/safedelete/category/', data={
@@ -165,5 +146,7 @@ class AdminTestCase(TestCase):
             'post': True,
             '_selected_action': [self.categories[1].pk],
         })
-        category = Category.objects.get(pk=self.categories[1].pk)
+        category = Category.objects.get(
+            pk=self.categories[1].pk
+        )
         self.assertFalse(category.deleted)
