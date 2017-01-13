1f933ae428ad32cd582fdc0da8331634248e2fd6
Gagaro <gagaro42@gmail.com>
2017-01-13 13:38:01 +0100

    prepare the 0.4.0 release

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'django-safedelete'
-copyright = '2017, Makina Corpus'
+copyright = '2017, palkeo, Makina Corpus'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
