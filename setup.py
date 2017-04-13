bae6a4906901740b1a8855709db717b3127e19f2
Gagaro <gagaro42@gmail.com>
2017-04-13 13:58:04 +0200

    fix: setup.py classifier

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Development Status :: 4',
+        'Development Status :: 4 - Beta',
     ],
     license='BSD',
     install_requires=['Django'],
