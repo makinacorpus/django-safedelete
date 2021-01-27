7a0bde09030c2de2331c507f3013fbe13a4ed1b9
Gagaro <gagaro42@gmail.com>
2021-01-27 15:04:30 +0100

    Added tests for subquery

---
+++ safedelete/tests/test_subquery.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+from safedelete.models import SafeDeleteMixin
+from safedelete.tests.testcase import SafeDeleteTestCase
+
+
+class Widget(models.Model):
+    pass
+
+
+class WidgetItem(SafeDeleteMixin):
+    widget = models.ForeignKey(
+        Widget,
+        on_delete=models.CASCADE,
+        related_name='items',
+    )
+
+
+class SubQueryTestCase(SafeDeleteTestCase):
+
+    def test_subquery_not_deleted(self):
+        widget = Widget.objects.create()
+        item = WidgetItem.objects.create(widget=widget)
+        item_queryset = WidgetItem.objects.values_list('widget').filter(id=item.id)
+        queryset = Widget.objects.filter(id__in=item_queryset)
+        self.assertEqual(len(list(queryset)), 1)
+
+    def test_subquery_deleted(self):
+        widget = Widget.objects.create()
+        item = WidgetItem.objects.create(widget=widget)
+        item.delete()
+        item_queryset = WidgetItem.objects.values_list('widget').filter(id=item.id)
+        queryset = Widget.objects.filter(id__in=item_queryset)
+        self.assertEqual(len(list(queryset)), 0)
