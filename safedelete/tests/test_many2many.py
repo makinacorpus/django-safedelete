e626c214d09db78a06e7be5c06c115db37ebc9ad
Gagaro <gagaro42@gmail.com>
2021-10-18 16:07:57 +0200

    Added failing test

--- safedelete/tests/test_many2many.py
+++ safedelete/tests/test_many2many.py
@@ -1,3 +1,5 @@
+import unittest
+
 from django.db import models
 
 from ..config import DELETED_VISIBLE
@@ -9,15 +11,45 @@ class ManyToManyChild(models.Model):
     pass
 
 
+class ManyToManyOtherChild(models.Model):
+    pass
+
+
+class ManyToManyOtherChildThrough(SafeDeleteModel):
+    other_child = models.ForeignKey(ManyToManyOtherChild, on_delete=models.CASCADE)
+    parent = models.ForeignKey('ManyToManyParent', on_delete=models.CASCADE)
+
+
 class ManyToManyParent(SafeDeleteModel):
     children = models.ManyToManyField(
         ManyToManyChild,
         blank=True,
         related_name='parents'
     )
+    other_children = models.ManyToManyField(
+        ManyToManyOtherChild,
+        blank=True,
+        related_name='parents',
+        through=ManyToManyOtherChildThrough,
+    )
 
 
 class ManyToManyTestCase(SafeDeleteTestCase):
+
+    @unittest.expectedFailure
+    def test_many_to_many_through(self):
+        """ This is not supported yet! """
+        parent = ManyToManyParent.objects.create()
+        other_child = ManyToManyOtherChild.objects.create()
+        through = ManyToManyOtherChildThrough.objects.create(other_child=other_child, parent=parent)
+
+        self.assertEqual(parent.manytomanyotherchildthrough_set.all().count(), 1)
+        self.assertEqual(parent.other_children.all().count(), 1)
+
+        through.delete()
+
+        self.assertEqual(parent.manytomanyotherchildthrough_set.all().count(), 0)
+        self.assertEqual(parent.other_children.all().count(), 0)
 
     def test_many_to_many(self):
         """Test whether related queries still works."""
