dc7bbc6d8084f81391ed8df30c311017d061053d
Gustavo Nagel <gust.nagel@gmail.com>
2022-03-25 21:20:24 -0300

    UPDATE: rewriting flatten method

--- safedelete/utils.py
+++ safedelete/utils.py
@@ -1,4 +1,4 @@
-from itertools import chain, filterfalse
+from itertools import chain
 
 from django.contrib.admin.utils import NestedObjects
 from django.db import router
@@ -14,32 +14,17 @@ def related_objects(obj, only_deleted_by_cascade=False):
     """
 
     collector = NestedObjects(using=router.db_for_write(obj))
-    collector.collect([obj])
-
-    def cascade_undelete_bypass(elem):
-        """ Test if elem should be bypassed by cascade undelete """
-
-        return elem != obj and getattr(elem, DELETED_BY_CASCADE_FIELD_NAME) is False
-
-    def replace_if_cascade_child(elem):
-        """ Wraps each item in elem with tuples including their cascading child classes """
-
-        if collector.edges.get(elem):
-            return (elem, collector.edges[elem])
-        else:
-            return (elem,)
+    collector.collect([obj])    
 
     def flatten(elem):
-        if isinstance(elem, list):
-            if only_deleted_by_cascade:
-                elem = filterfalse(cascade_undelete_bypass, elem)
-            expanded_elem = chain.from_iterable(map(replace_if_cascade_child, elem))
-            return chain.from_iterable(map(flatten, expanded_elem))
-        elif obj != elem:
-            return (elem,)
+        if isinstance(elem, tuple):
+            return elem
+        elif obj == elem or not only_deleted_by_cascade or getattr(elem, DELETED_BY_CASCADE_FIELD_NAME):
+            elem = [(elem,) if elem != obj else (), *collector.edges.get(elem, [])]
+            return chain.from_iterable(map(flatten, elem))
         return ()
 
-    return flatten([obj])
+    return flatten(obj)
 
 
 def can_hard_delete(obj):
